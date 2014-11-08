a975c9d464cab0134c9f35ae7640590fd02f7434
Maria Korosteleva <mariak@seclab.cs.msu.su>
2014-11-08 15:54:22 +0400

    output

--- task2/par.c
+++ task2/par.c
@@ -1,18 +1,20 @@
-#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <mpi.h>
+#include <math.h>
 
 #define Max(a,b) ((a)>(b)?(a):(b))
-#define N 256
+#define PI 3.14159265
 
-double maxeps = 0.1e-7;
-int itmax = 100;
+double maxeps = 0.01;
+int N = 512;
+// spectral radius
+int itmax = 10000;
 int proc_size;
 int proc_rank;
 double eps;
-double U[N][N], U_dop[N][N];
+double** U = NULL;
 void relax();
 void init();
 void verify();
@@ -28,21 +30,33 @@ void wtime(double *t)
 	*t = (tv.tv_sec - sec) + 1.0e-6*tv.tv_usec;
 }
 
+double omega(int n)
+{
+    double p = 1. - (PI / ((N - 1)))*(PI / ((N - 1))); 
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+	{
+		return 1./(1. - p*p/2); 
+	}
+	else
+	{
+		return 1./(1. - omega(n-1)*p*p/4); 
+	}
+}
 
-void relax()
+// parameter: iteration number
+void relax(int iter)
 {
     int i, j;
-	for(i=1; i<=N-2; i++)
-        for(j=1; j<=N-2; j++)
+	double om = omega(iter);
+	for(i=1; i < N+1; i++)
+        for(j=1; j< N+1; j++)
         {
-            U[i][j] = (U[i-1][j] + U[i+1][j])/2.;
-        }
-    for(i=1; i<=N-2; i++)
-        for(j=1; j<=N-2; j++)
-        {
-            U[i][j] = (U[i][j-1] + U[i][j+1])/2.;
+            double U_dop = (U[i-1][j] + U[i+1][j] + U[i][j-1] + U[i][j+1])/4.;
 			double e;
 			e = U[i][j];
+            U[i][j] = om * U_dop + (1. - om) * U[i][j];
 			eps = eps > fabs(e - U[i][j]) ? eps : fabs(e - U[i][i]);
         }
 }
@@ -52,8 +66,8 @@ void verify()
 	double s;
 	s = 0.;
 	int i, j;
-	for(i = 0; i <= N-1; i++)
-		for(j = 0; j <= N-1; j++)
+	for(i = 0; i < N+2; i++)
+		for(j = 0; j < N+2; j++)
 			{
 				s = s + U[i][j] * (i + 1) * (j + 1) / (N * N);
 			}
@@ -64,20 +78,73 @@ void verify()
 void init()
 {
 	int i, j;
-    for(i=0; i<=N-1; i++)
-        for(j=0; j<=N-1; j++)
+	U = calloc(N + 2, sizeof(U[0]));
+	for (i = 0; i < N + 2; i++) 
+	{
+		U[i] = calloc(N + 2, sizeof(U[i][0]));
+	}
+    for(i=0; i< N+2; i++)
+        for(j=0; j< N+2; j++)
         { 
-            if(i == 0 || i == N - 1 || j == 0 || j == N - 1)
-                U[i][j] = 0.;
-            else 
-				U[i][j] = ( 4. + i + j) ;
+			double x = i * (1.0 / (N + 1));
+			double y = j * (1.0 / (N + 1));
+			
+			if(j == 0) 
+            {    
+				U[i][j] = sin(PI * x / 2);
+            }
+			else if (j == N + 1)
+			{
+				U[i][j] = sin(PI * x / 2) * exp(-PI/2);
+			}
+			else if (i == N + 1)
+			{
+				U[i][j] = exp(- PI * y / 2);
+			}
+			else 
+				U[i][j] = 0 ;
         }
 }
 
 
-int main(int an, char **as)
+void output(const char* name)
+{
+	// write matrix to file
+	FILE* grid = fopen(name, "w");
+	int i, j;
+	for (i = 0; i < N+2; i++)
+	{
+		for (j = 0; j < N+2; j++)
+		{
+			fprintf(grid, "%lf ", U[i][j]);
+		}
+		fprintf(grid, "\n");
+	}
+	flose(grid);
+}
+
+int main(int argc, char ** argv)
 { 
-	int err = MPI_Init(&an, &as);
+	if (argc > 1)
+	{
+		sscanf(argv[1], "%d", &N);
+	}
+	if (N <= 0)
+	{
+		printf("Error: grid size is too small\n");
+		return 3;
+	}
+	if (argc > 2)
+	{
+		sscanf(argv[2], "%lf", &maxeps);
+	}
+	if (maxeps <= 0)
+	{
+		printf("Error: epsilon is too small\n");
+		return 4;
+	}
+	
+	int err = MPI_Init(&argc, &argv);
 	if (err != MPI_SUCCESS)
 	{
 		fprintf(stderr, "Error: MPI_Init. Aborting...\n");
@@ -85,32 +152,39 @@ int main(int an, char **as)
 	}
     MPI_Comm_size(MPI_COMM_WORLD, &proc_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
-    //proc_size = MPI_Group_size(MPI_COMM_WORLD, &proc_size);
-    //proc_rank = MPI_Group_rank(MPI_COMM_WORLD, &proc_rank);
 	
 // we define process with rank 0 as the main one
     double start, fin;
 	if (proc_rank == 0)
 		wtime(&start);
-	int it;
     init();
 
 	printf( "Hello world from process %d of %d\n", proc_rank, proc_size );
     
-	for(it=1; it<=itmax; it++)
+	int it;
+	for(it = 1; it < itmax; it++)
     {
         eps = 0.;
-        relax();
+        relax(it);
 		
-        printf( "it=%4i eps=%f\n", it,eps);
+        printf( "it=%4i eps=%f\n", it, eps);
         if (eps < maxeps) break;
     }
 	if (proc_rank == 0)
     {
 		wtime(&fin);
 		printf("Time in seconds=%gs\t", fin - start);
+		if (argc > 3)
+		{
+			output(argv[2]);
+		}
+		else 
+		{
+			output("grid.txt");
+		}
 	}
     verify();
+
 	
 	err = MPI_Finalize();
 	if (err)
@@ -119,5 +193,11 @@ int main(int an, char **as)
 		fprintf(stderr, "Error: MPI_Finalize. Aborting...\n");
 		return 2;
 	}
+	int i;
+	for (i = 0; i < N + 2; ++i) 
+	{
+		free(U[i]);
+	}
+	free(U);
     return 0;
 }
